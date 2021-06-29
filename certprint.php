edbe3a01194290139b9c4cd35883889bc79f61f6
Alexander Popov <alexandpopov@yandex.ru>
2021-06-29 10:36:37 +0300

    Update certprint.php

--- certprint.php
+++ certprint.php
@@ -1,7 +1,9 @@
 <?php
+/*
+Это отрывок кода, из метода, печатающего сертификат. Метод принимает объекты $student, $cert(ificate). Дополнительные данные о студентах хранятся в json файлах в защищенной папке.
+*/
 $color = imagecolorallocate($img, 0, 0, 0);
 $color1 = imagecolorallocate($img, 255, 0, 0);
-// указываем путь к шрифту
 $font = 'assets/fonts/arialmt.ttf';
 $font1 = 'assets/fonts/arial-boldmt.ttf';
 $font2='assets/fonts/ofont.ru_Garamond cond.ttf';
@@ -30,26 +32,14 @@ imagettftext($img, 35, 0, 1936, 756, $color, $font3, $passed);
 imagettftext($img, 40, 0, 2073, 900, $color, $font5, 'Богословской Семинарии ');
 imagettftext($img, 35, 0,1990, 1026, $color, $font5, 'по программе дополнительного образования ');
 $name=$student->lastname.' '.$student->name.' '.$student->fname;
-
-
-if(false){
-imagettftext($img, 37, 0, 2273, 530, $color, $font1, $dlname);
-imagettftext($img, 37, 0, 2186, 613, $color, $font1, $dname);
-}else{
-    
-imagettftext($img, 43, 0, 2073, 590, $color, $font2, $name);
-    
-}
+imagettftext($img, 43, 0, 2073, 590, $color, $font2, $name); 
 $vert=1168;
-foreach($tar as $v){
-  
+foreach($tar as $v){ 
 imagettftext($img, 45, 0, 1990, $vert, $color, $font2, $v);
 $vert+=75;   
 }
 imagettftext($img, 35, 0, 2584, 1782, $color, $font4, RECTOR);
 imagettftext($img, 35, 0, 2570, 1882, $color, $font4, SECRETARY);
 imagettftext($img, 35, 0, 2140, 1882, $color, $font3, 'МП');
-
 imagettftext($img, 35, 0, 1936, 1437, $color, $font3, 'в объеме '.$cert->hours.' академических часов');
-
 ?>
